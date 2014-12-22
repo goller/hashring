d8a7abd16e115e142299a4c1ed01b18b15a5b806
Chris Goller <goller@gmail.com>
2014-12-22 11:34:15 -0600

    Add additional test for strings

--- tests/test_hashring.py
+++ tests/test_hashring.py
@@ -5,3 +5,14 @@ def test_basic_ring():
     actual = hr.get_node('howdy')
     expected = 1
     assert expected == actual
+
+
+def test_server_ring():
+    memcache_servers = ['192.168.0.246:11212',
+                        '192.168.0.247:11212',
+                        '192.168.0.249:11212']
+
+    ring = HashRing(memcache_servers)
+    actual = ring.get_node('my_key')
+    expected = '192.168.0.247:11212'
+    assert expected == actual
